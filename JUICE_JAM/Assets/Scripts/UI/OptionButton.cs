64ea26369fb5de232d13bf16eb67436abe5fe85b
Robin Six <six.robin@hotmail.fr>
2022-01-09 00:35:39 +0100

    pause menu WIP

--- JUICE_JAM/Assets/Scripts/UI/OptionButton.cs
+++ JUICE_JAM/Assets/Scripts/UI/OptionButton.cs
@@ -3,13 +3,62 @@ namespace JuiceJam.UI
     using UnityEngine;
     using UnityEngine.EventSystems;
 
-    public class OptionButton : MonoBehaviour, ISelectHandler
+    public class OptionButton : MonoBehaviour, ISelectHandler, IDeselectHandler
     {
         [SerializeField] private RSLib.Framework.GUI.EnhancedButton _button = null;
+        [SerializeField] private System.Collections.Generic.List<GameObject> _selectionArrows = null;
+        [SerializeField] private TMPro.TextMeshProUGUI _buttonText = null;
+        [SerializeField] private Color _selectedColor = Color.white;
+
+        private Color _baseColor;
+
+        public static event System.Action<OptionButton> ButtonHovered;
+
+        public void OnSelect()
+        {
+            OnSelect(null);
+        }
 
         public void OnSelect(BaseEventData eventData)
         {
-            _button.OnPointerEnter(null);
+            _selectionArrows.ForEach(o => o.SetActive(true));
+            _buttonText.color = _selectedColor;
+        }
+
+        public void OnDeselect()
+        {
+            OnDeselect(null);
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            _selectionArrows.ForEach(o => o.SetActive(false));
+            _buttonText.color = _baseColor;
+        }
+
+        private void OnPointerEnter(RSLib.Framework.GUI.EnhancedButton source)
+        {
+            ButtonHovered?.Invoke(this);
+        }
+
+        private void OnButtonHovered(OptionButton button)
+        {
+            if (button != this)
+                OnDeselect();
+        }
+
+        private void Awake()
+        {
+            _baseColor = _buttonText.color;
+
+            _button.PointerEnter += OnPointerEnter;
+            ButtonHovered += OnButtonHovered;
+        }
+
+        private void OnDestroy()
+        {
+            _button.PointerEnter -= OnPointerEnter;
+            ButtonHovered -= OnButtonHovered;
         }
     }
 }
--- JUICE_JAM/Assets/Scripts/UI/OptionsPanel.cs
+++ JUICE_JAM/Assets/Scripts/UI/OptionsPanel.cs
@@ -7,9 +7,34 @@ namespace JuiceJam.UI
     {
         [SerializeField] private RSLib.Framework.GUI.EnhancedButton[] _buttons = null;
         [SerializeField] private Canvas _canvas = null;
+        [SerializeField] private float _resumeDelay = 0.4f;
+
+        [Header("BUTTONS SPECS")]
+        [SerializeField] private RSLib.Framework.GUI.EnhancedButton _resumeButton = null;
+        [SerializeField] private RSLib.Framework.GUI.EnhancedButton _exitButton = null;
 
         public static bool PausingCoroutineRunning { get; private set; }
         public static bool IsOpen { get; private set; }
+
+        private void OnResumeButtonClicked()
+        {
+            if (!IsOpen || PausingCoroutineRunning)
+                return;
+
+            TogglePause();
+        }
+
+        private void OnExitButtonClicked()
+        {
+            if (!IsOpen || PausingCoroutineRunning)
+                return;
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            UnityEngine.Application.Quit();
+#endif
+        }
 
         private void InitNavigation()
         {
@@ -21,8 +46,47 @@ namespace JuiceJam.UI
             }
         }
 
+        private void TogglePause()
+        {
+            IsOpen = !IsOpen;
+            StartCoroutine(TogglePauseCoroutine(IsOpen));
+        }
+
+        private System.Collections.IEnumerator TogglePauseCoroutine(bool pausing)
+        {
+            PausingCoroutineRunning = true;
+
+            if (pausing)
+            {
+                TimeManager.SetTimeScale(0f);
+
+                DitherFade.FadeIn(0.3f, RSLib.Maths.Curve.Linear);
+                yield return new WaitUntil(() => !DitherFade.IsFading);
+                yield return RSLib.Yield.SharedYields.WaitForSecondsRealtime(0.2f);
+
+                _canvas.enabled = true;
+
+                UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(_buttons[0].gameObject);
+            }
+            else
+            {
+                _canvas.enabled = false;
+
+                DitherFade.FadeOut(0.3f, RSLib.Maths.Curve.Linear);
+                yield return new WaitUntil(() => !DitherFade.IsFading);
+                yield return RSLib.Yield.SharedYields.WaitForSecondsRealtime(_resumeDelay);
+
+                TimeManager.SetTimeScale(1f);
+            }
+
+            PausingCoroutineRunning = false;
+        }
+
         private void Start()
         {
+            _resumeButton.onClick.AddListener(OnResumeButtonClicked);
+            _exitButton.onClick.AddListener(OnExitButtonClicked);
+
             InitNavigation();
             _canvas.enabled = false;
         }
@@ -33,37 +97,14 @@ namespace JuiceJam.UI
                 && !DitherFade.IsFading
                 && Input.GetButtonDown("Pause"))
             {
-                IsOpen = !IsOpen;
-                StartCoroutine(TogglePauseCoroutine(IsOpen));
+                TogglePause();
             }
         }
 
-        private System.Collections.IEnumerator TogglePauseCoroutine(bool pausing)
+        private void OnDestroy()
         {
-            PausingCoroutineRunning = true;
-
-            if (pausing)
-            {
-                TimeManager.SetTimeScale(0f);
-                
-                DitherFade.FadeIn(0.3f, RSLib.Maths.Curve.Linear);
-                yield return new WaitUntil(() => !DitherFade.IsFading);
-                yield return RSLib.Yield.SharedYields.WaitForSecondsRealtime(0.2f);
-
-                _canvas.enabled = true;
-            }
-            else
-            {
-                _canvas.enabled = false;
-
-                DitherFade.FadeOut(0.3f, RSLib.Maths.Curve.Linear);
-                yield return new WaitUntil(() => !DitherFade.IsFading);
-                yield return RSLib.Yield.SharedYields.WaitForSecondsRealtime(0.2f);
-
-                TimeManager.SetTimeScale(1f);
-            }
-
-            PausingCoroutineRunning = false;
+            _resumeButton.onClick.RemoveListener(OnResumeButtonClicked);
+            _exitButton.onClick.RemoveListener(OnExitButtonClicked);
         }
     }
 }
