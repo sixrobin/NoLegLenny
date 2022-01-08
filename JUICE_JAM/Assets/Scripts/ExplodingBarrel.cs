c8c6121f503716772909cd81fe17a3704b18c25b
Robin Six <six.robin@hotmail.fr>
2022-01-08 15:34:24 +0100

    Barrels chain explosion

--- JUICE_JAM/Assets/Scripts/ExplodingBarrel.cs
+++ JUICE_JAM/Assets/Scripts/ExplodingBarrel.cs
@@ -2,7 +2,7 @@ namespace JuiceJam
 {
     using UnityEngine;
 
-    public class ExplodingBarrel : MonoBehaviour, IDamageable, IRespawnable
+    public class ExplodingBarrel : MonoBehaviour, IDamageable, IExplodable, IRespawnable
     {
         [Header("REFS")]
         [SerializeField] private Collider2D _collider2D = null;
@@ -10,6 +10,7 @@ namespace JuiceJam
 
         [Header("EXPLOSION")]
         [SerializeField, Min(0f)] private float _explosionRadius = 3f;
+        [SerializeField, Min(0f)] private float _barrelChainRadius = 2.5f;
         [SerializeField, Min(0f)] private float _explosionForce = 10f;
         [SerializeField, Min(0f)] private float _explosionForceMin = 3f;
 
@@ -19,6 +20,7 @@ namespace JuiceJam
         [SerializeField, Min(0f)] private float _freezeFrameDuration = 0f;
         [SerializeField, Min(0)] private int _freezeFrameDelay = 0;
         [SerializeField] private Transform _ripplePosition = null;
+        [SerializeField] private float _barrelChainDelay = 0.2f;
 
         public bool CanBeDamaged => true;
         public bool DontDestroyDamageSource => false;
@@ -29,6 +31,14 @@ namespace JuiceJam
             _animator.SetTrigger("Explode");
         }
 
+        public void Explode(ExplosionData explosionData)
+        {
+            if (!_collider2D.enabled)
+                return;
+
+            StartCoroutine(ChainBarrelExplosion());
+        }
+
         public void Respawn()
         {
             _collider2D.enabled = true;
@@ -36,7 +46,7 @@ namespace JuiceJam
         }
 
         [ContextMenu("Explode")]
-        public void Explode()
+        public void OnExplosionFrame()
         {
             ExplosionData explosionData = new ExplosionData()
             {
@@ -46,10 +56,31 @@ namespace JuiceJam
                 Source = transform.position
             };
 
+            ExplosionData barrelChainExplosionData = new ExplosionData()
+            {
+                Force = _explosionForce,
+                ForceMin = _explosionForceMin,
+                Radius = _barrelChainRadius,
+                Source = transform.position
+            };
+
             Collider2D[] nearColliders = Physics2D.OverlapCircleAll(transform.position, _explosionRadius);
+
             for (int i = nearColliders.Length - 1; i >= 0; --i)
+            {
                 if (nearColliders[i].TryGetComponent(out IExplodable explodable))
-                    explodable.Explode(explosionData);
+                {
+                    if (explodable is ExplodingBarrel)
+                    {
+                        if ((nearColliders[i].transform.position - transform.position).sqrMagnitude < _barrelChainRadius * _barrelChainRadius)
+                            explodable.Explode(barrelChainExplosionData);
+                    }
+                    else
+                    {
+                        explodable.Explode(explosionData);
+                    }
+                }
+            }
 
             FreezeFrameManager.FreezeFrame(_freezeFrameDelay, _freezeFrameDuration);
             CameraShake.AddTrauma(_destroyedTrauma);
@@ -59,10 +90,18 @@ namespace JuiceJam
                 _explosionParticlesSystems[i].Play();
         }
 
+        private System.Collections.IEnumerator ChainBarrelExplosion()
+        {
+            yield return RSLib.Yield.SharedYields.WaitForSeconds(_barrelChainDelay);
+            TakeDamage(new());
+        }
+
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(transform.position, _explosionRadius);
+            Gizmos.color = Color.Lerp(Color.yellow, Color.red, 0.75f);
+            Gizmos.DrawWireSphere(transform.position, _barrelChainRadius);
         }
     }
 }
